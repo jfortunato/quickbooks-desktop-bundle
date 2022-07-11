720bdd3262e4fa30031ca9311a790665ee78c857
Justin Fortunato <jfortunato25@gmail.com>
2022-07-11 15:16:26 -0400

    Use controller service arguments

--- Controller/QuickBooksDesktopController.php
+++ Controller/QuickBooksDesktopController.php
@@ -2,17 +2,17 @@
 
 namespace Jfortunato\QuickBooksDesktopBundle\Controller;
 
+use Jfortunato\QuickBooksDesktopBundle\Wrapper\Server;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class QuickBooksDesktopController extends Controller
 {
-    public function qbwcAction()
+    public function qbwcAction(Server $server)
     {
         // Set up our queue singleton
         \QuickBooks_WebConnector_Queue_Singleton::initialize($this->container->get('jfortunato_quick_books_desktop.config')->getDsn());
 
-        $server = $this->container->get('jfortunato_quick_books_desktop.wrapper.server');
         $handled = $server->handle(true, true);
 
         $response = new Response;
